2b0bea54cf46ca62c8a9a862d87c3355af1299ce
Niklas Ekman <niklas.ekman@gmail.com>
2017-08-30 10:53:04 +0200

    Switched to fork in order to solve bug in cpanel

--- internal_build.sh
+++ internal_build.sh
@@ -5,6 +5,6 @@ apk add --update git && rm -rf /var/cache/apk/*
 # pull the code for the cpanel-update-dns
 mkdir -p /opt
 cd /opt
-git clone "https://github.com/birkoss/cPanel-Update-Dns.git" cpanel-update-dns
+git clone "https://github.com/oakman/cPanel-Update-Dns.git" cpanel-update-dns
 cd /opt/cpanel-update-dns
 apk del git
